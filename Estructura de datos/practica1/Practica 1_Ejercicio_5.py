07323af49865e3d2f0e7dd20f88113a8629fcc40
LoraShan <loraraylin@gmail.com>
2025-05-08 01:55:32 -0400

    Create Practica 1_Ejercicio_5.py

---
+++ Estructura de datos/practica1/Practica 1_Ejercicio_5.py
@@ -0,0 +1,8 @@
+# Solicitar al usuario que ingrese su edad
+edad = int(input("Por favor, ingresa tu edad: "))
+
+# Determinar si es mayor de edad y mostrar el resultado
+if edad >= 18:
+    print("¡Eres mayor de edad!")
+else:
+    print("Aún no eres mayor de edad.")
