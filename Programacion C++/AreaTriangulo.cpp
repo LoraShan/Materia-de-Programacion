bada469ad8ea1e6e9a57c8c2755cc16de1415bc3
LoraShan <loraraylin@gmail.com>
2025-05-08 01:28:41 -0400

    Create AreaTriangulo.cpp

---
+++ Programacion C++/AreaTriangulo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float base, altura;
+    cout << "Ingrese base y altura: ";
+    cin >> base >> altura;
+    cout << "El area del triangulo es: " << (base * altura) / 2 << endl;
+    return 0;
+}
