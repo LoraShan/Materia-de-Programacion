95c4edf2d3d88763c4cb15acf65d2d1bdb6f8624
LoraShan <loraraylin@gmail.com>
2025-05-08 01:55:49 -0400

    Create Practica 2_Ejercicio_1.py

---
+++ Estructura de datos/practica2/Practica 2_Ejercicio_1.py
@@ -0,0 +1,5 @@
+# Definir la tupla con 5 países
+paises = ('Republica Dominicana', 'Haiti', 'Japon', 'China', 'Escocia')
+
+# Mostrar el tercer elemento (índice 2 en Python)
+print("El tercer país en la tupla es:", paises[2])
